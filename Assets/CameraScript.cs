8f28d79e80b1cb9c4c9cc33a106d015879e90007
pp <a.armar2003@fmail.com>
2023-02-03 18:38:46 +0100

    basics cam mov + cinemachine

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public int sensitivity;
+    public float camSpeed;
+    float Ypos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ypos += Input.GetAxis("Mouse ScrollWheel") * sensitivity * Time.deltaTime;
+        Vector3 vec = new Vector3(transform.position.x, Ypos, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, vec, camSpeed);
+    }
+}
