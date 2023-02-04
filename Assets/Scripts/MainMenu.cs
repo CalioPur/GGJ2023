7e57cdd4e63a89748099ea01a58e5b57e8853cfa
TOUL Xochil <nix.axius@gmail.com>
2023-02-04 17:57:49 +0100

    Main menu works (need to write how to play + credits)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject creditsPanel;
+    public GameObject HtPPanel;
+
+    public void Quit()
+    {
+        print("quitting");
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void CreditsOpen()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void HowToPlayOpen()
+    {
+        HtPPanel.SetActive(true);
+    }
+
+    public void CreditsClose()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void HowToPlayClose()
+    {
+        HtPPanel.SetActive(false);
+    }
+}
