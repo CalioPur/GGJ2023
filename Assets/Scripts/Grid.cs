3d5dcf86e15161725d5beb48c71e265a84a2ccb9
TOUL Xochil <nix.axius@gmail.com>
2023-02-04 14:48:52 +0100

    grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -22,8 +22,6 @@ public class Grid
         {
             for (int y = 0; y < this.gridArray.GetLength(1); y++)
             {
-                Debug.Log(x + " " + y);
-                Debug.Log(GetWorldPosition(x, y));
                 Debug.DrawLine(GetWorldPosition(x,y),GetWorldPosition(x,y+1), Color.red, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y), Color.red, 100f);
             }
@@ -35,5 +33,27 @@ public class Grid
     private Vector3 GetWorldPosition(int x, int y)
     {
         return new Vector3(x,y) * this.cellSize;
+    }
+
+    private void GetXY(Vector3 worldPos, out int x, out int y)
+    {
+        x = Mathf.FloorToInt(worldPos.x / this.cellSize);
+        y = Mathf.FloorToInt(worldPos.y / this.cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >=0 && y>=0 && x<this.width && y < this.height)
+        {
+            this.gridArray[x, y] = value;
+            Debug.Log(x + " " + y);
+        }
+    }
+
+    public void SetValue(Vector3 worldPos, int val)
+    {
+        int x, y;
+        GetXY(worldPos, out x, out y);
+        SetValue(x, y, val);
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    private Grid grid;
     private void Start()
     {
-        Grid grid = new Grid(50, 20, 0.25f);
+        grid = new Grid(85, 20, 0.25f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 10);
+        }
     }
 }
